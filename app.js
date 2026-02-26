7b5f1eb3e4bb69d7ff10162308c0ba1824000f3c
Gisela Lucena <lucena.gisela@gmail.com>
2026-02-26 10:12:02 -0500

    adds middleware for routes not found

--- app.js
+++ app.js
@@ -10,6 +10,13 @@ app.use(express.json());
 app.use('/users', userRouter);
 app.use('/cards', cardRouter);
 
+// Middleware para rotas não encontradas
+app.use((req, res) => {
+  res.status(404).json({ 
+    message: 'Rota não encontrada' 
+  });
+});
+
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
